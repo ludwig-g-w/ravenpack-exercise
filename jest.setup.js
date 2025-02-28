ecb7acc63d7e16d35affcd1ecefe9d864abc64a5
ludwigsw <ludwig.wiren@rocker.coml>
2025-02-28 21:07:22 +0100

    init test index.tsx

--- jest.setup.js
+++ jest.setup.js
@@ -1,41 +1,9 @@
-jest.mock("react-native/Libraries/Utilities/Dimensions", () => {
-  return {
-    get: jest.fn().mockReturnValue({
-      width: 375,
-      height: 812,
-    }),
-  };
-});
+// // Mock expo-router
+// jest.mock("expo-router", () => ({
+//   useRouter: () => ({
+//     push: jest.fn(),
+//   }),
+// }));
 
-// Mock the Animated component from react-native-reanimated
-jest.mock("react-native-reanimated", () => {
-  const Reanimated = require("react-native-reanimated/mock");
-  // Mock the SharedTransition functions
-  Reanimated.SharedTransition = {
-    custom: () => ({
-      progressAnimation: () => ({
-        defaultTransitionType: jest.fn(),
-      }),
-    }),
-  };
-  Reanimated.SharedTransitionType = {
-    ANIMATION: "ANIMATION",
-  };
-  return Reanimated;
-});
-
-// Mock expo-router
-jest.mock("expo-router", () => ({
-  useRouter: () => ({
-    push: jest.fn(),
-  }),
-}));
-
-jest.mock("react-native-safe-area-context", () => ({
-  useSafeAreaInsets: () => ({
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  }),
-}));
+// // Add React import
+// const React = require("react");
