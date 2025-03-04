20b8911f0c7e4055d96e3633ea47a424fdfa6191
ludwigsw <ludwig.wiren@rocker.coml>
2025-03-04 09:11:57 +0100

    Add Tailwind container and responsive layout wrapper
    - Wrapped Stack navigation in a Tailwind CSS container for better responsiveness
    - Added max-width and auto margin to center content on larger screens
    - Imported View from react-native to enable container wrapping

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,36 +2,38 @@ import Providers from "@/providers";
 import { Stack } from "expo-router";
 import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
 import "../global.css";
-import { Platform } from "react-native";
+import { Platform, View } from "react-native";
 
 export default function Layout() {
   return (
     <Providers>
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{
-            ...commonOptions,
-            title: "Blog Posts",
-            headerLargeTitleShadowVisible: false,
-            headerLargeTitle: true,
-          }}
-        />
-        <Stack.Screen
-          name="posts/[id]"
-          options={{
-            ...commonOptions,
-            title: "Post",
-          }}
-        />
-        <Stack.Screen
-          name="users/[id]"
-          options={{
-            ...commonOptions,
-            title: "User",
-          }}
-        />
-      </Stack>
+      <View className="flex-1 container max-w-screen-lg mx-auto ">
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              ...commonOptions,
+              title: "Blog Posts",
+              headerLargeTitleShadowVisible: false,
+              headerLargeTitle: true,
+            }}
+          />
+          <Stack.Screen
+            name="posts/[id]"
+            options={{
+              ...commonOptions,
+              title: "Post",
+            }}
+          />
+          <Stack.Screen
+            name="users/[id]"
+            options={{
+              ...commonOptions,
+              title: "User",
+            }}
+          />
+        </Stack>
+      </View>
     </Providers>
   );
 }
